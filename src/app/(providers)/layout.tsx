832edfa08d8630392ac8fa8b82d7135e3aa04300
baeseongjae <3574214@naver.com>
2024-04-02 21:18:15 +0900

    ✨ feat: authContext 생성

--- src/app/(providers)/layout.tsx
+++ src/app/(providers)/layout.tsx
@@ -1,10 +1,11 @@
+import { AuthProvider } from "@/contexts/auth.context";
 import ReactQueryProvider from "@/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ReactQueryProvider>
-      {children}
+      <AuthProvider>{children}</AuthProvider>
       <ReactQueryDevtools />
     </ReactQueryProvider>
   );
