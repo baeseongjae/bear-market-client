4054e1a483dfddc3cd1fe3202b074a04f3d87932
baeseongjae <3574214@naver.com>
2024-04-02 23:23:59 +0900

    ✨ feat: 모달 이벤트 버블링 제어

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -15,6 +15,7 @@ function Modal({ children, className }: PropsWithChildren<ModalProps>) {
       className="bg-black/50 flex items-center justify-center fixed top-0 left-0 right-0 bottom-0 z-20"
     >
       <div
+        onClick={(e) => e.stopPropagation()}
         className={`bg-white rounded-md w-full max-w-[400px] px-5 py-8 ${className}`}
       >
         {children}
