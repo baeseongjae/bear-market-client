132cddf1e6a985b7f87878c4d62a2804310d7fb7
baeseongjae <3574214@naver.com>
2024-04-16 03:31:20 +0900

    ✨ feat: 폼데이터 처리 util 함수 구현

---
+++ src/utils/useFormData.util.ts
@@ -0,0 +1,14 @@
+import { DealForm } from "@/types/DealForm.type";
+
+export function useFormData(dealFormData: DealForm) {
+  const { image, ...dealFormDataNotImage } = dealFormData;
+
+  const formData = new FormData();
+
+  Object.entries(dealFormDataNotImage).forEach(([key, value]) => {
+    formData.append(key, value);
+  });
+  if (image) formData.append("image", image);
+
+  return formData;
+}
