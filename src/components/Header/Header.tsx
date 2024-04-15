c52c75e2055196fa862b7ab260719966f39d3bd6
baeseongjae <3574214@naver.com>
2024-04-16 03:33:24 +0900

    🐛 fix: 스크롤시 정렬 버튼 최상단으로 올라오는 이슈해결 - 헤더 z-index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,7 +4,7 @@ import HeaderSignWrapper from "./components/HeaderSignWrapper";
 
 function Header() {
   return (
-    <header className="bg-white border-b shadow-xl px-8 sticky top-0">
+    <header className="bg-white border-b shadow-xl px-8 sticky z-10 top-0">
       <div className="max-w-screen-lg flex items-center mx-auto h-16">
         <HeaderLogo />
         <HeaderMenu />
