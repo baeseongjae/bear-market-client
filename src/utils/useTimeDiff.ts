9cdd8a7014ba5ee35ae89cc09689d8020bfc59bd
baeseongjae <3574214@naver.com>
2024-04-17 21:08:18 +0900

    ♻️ refactor: useTimeDiff 유틸함수 - week, month, year 계산 추가

--- src/utils/useTimeDiff.ts
+++ src/utils/useTimeDiff.ts
@@ -1,7 +1,10 @@
 import {
+  differenceInDays,
   differenceInHours,
   differenceInMinutes,
-  formatDistanceToNow,
+  differenceInMonths,
+  differenceInWeeks,
+  differenceInYears,
   parseISO,
 } from "date-fns";
 
@@ -23,5 +26,21 @@ export function useTimeDiff(createdAt: string): string {
     return `${diffHours}시간 전`;
   }
 
-  return formatDistanceToNow(date, { addSuffix: true });
+  const diffDays = differenceInDays(now, date);
+  if (diffDays < 7) {
+    return `${diffDays}일 전`;
+  }
+
+  const diffWeeks = differenceInWeeks(now, date);
+  if (diffWeeks < 5) {
+    return `${diffWeeks}주 전`;
+  }
+
+  const diffMonths = differenceInMonths(now, date);
+  if (diffMonths < 12) {
+    return `${diffMonths}달 전`;
+  }
+
+  const diffYears = differenceInYears(now, date);
+  return `${diffYears}년 전`;
 }
