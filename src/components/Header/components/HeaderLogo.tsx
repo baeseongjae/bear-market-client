65f69f0a1a50b193c93431c7a222cbc4b84c5353
baeseongjae <3574214@naver.com>
2024-04-20 21:34:51 +0900

    ♻️ refactor: outline color 변경

--- src/components/Header/components/HeaderLogo.tsx
+++ src/components/Header/components/HeaderLogo.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import { GiBearFace } from "react-icons/gi";
+
 function HeaderLogo() {
   return (
     <h1 className="text-3xl font-black">
-      <Link href="/" className="flex gap-x-1 text-primary-100">
+      <Link
+        href="/"
+        className="flex gap-x-1 text-primary-100 rounded-lg outline-primary-100"
+      >
         <span className="text-[58px] lg:text-2xl">
           <GiBearFace />
         </span>
--- src/components/Header/components/HeaderMenu.tsx
+++ src/components/Header/components/HeaderMenu.tsx
@@ -17,12 +17,12 @@ function HeaderMenu() {
   };
 
   return (
-    <nav className="xs:ml-12 ml-5 hidden xs:block">
+    <nav className="xs:ml-12 ml-5 hidden xs:block ">
       <ul className="flex items-center">
         <li>
           <Link
             href="/"
-            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg"
+            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg outline-primary-100"
           >
             둘러보기
           </Link>
@@ -33,7 +33,7 @@ function HeaderMenu() {
             onClick={() => {
               handleClickServiceNeedLogIn("/deals/create");
             }}
-            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg"
+            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg outline-primary-100"
           >
             판매하기
           </Link>
@@ -41,7 +41,7 @@ function HeaderMenu() {
         <li>
           <Link
             href={"#"}
-            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg"
+            className="hover:bg-primary-100 hover:text-white px-3 py-2 rounded-lg outline-primary-100"
           >
             채팅하기
           </Link>
