a04cdb84f0849d0d588702a38e51f329b45fb71e
baeseongjae <3574214@naver.com>
2024-04-20 21:45:15 +0900

    ♻️ refactor: 판매글 리스트 모바일 반응형 ui 추가구현

--- src/components/Page/Page.tsx
+++ src/components/Page/Page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Page({ children }: { children: React.ReactNode }) {
   return (
-    <main className="box-border max-w-screen-lg px-8 pt-12 mx-auto">
+    <main className="box-border max-w-screen-lg px-8 pt-5 mx-auto">
       {children}
     </main>
   );
