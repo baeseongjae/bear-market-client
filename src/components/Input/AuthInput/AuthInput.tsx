93a86d36658e74264588394bdcf8a36af0b36ad4
baeseongjae <3574214@naver.com>
2024-04-20 18:50:42 +0900

    🐛 fix: focus 풀리면 input에 값이 있더라도 label이 내려오는 문제 해결

--- src/components/Input/AuthInput/AuthInput.tsx
+++ src/components/Input/AuthInput/AuthInput.tsx
@@ -13,17 +13,17 @@ function AuthInput({ label, className, ...props }: AuthInputProps) {
     <>
       <input
         id={id}
-        className={`h-12 border border-slate-300 text-xs xs:text-[13px] md:text-sm lg:text-base
+        className={`h-12 border border-slate-300 text-xs xs:text-[13px] md:text-sm lg:text-sm
         focus:border-primary-100 outline-none transition rounded-lg pl-4 peer ${className}`}
         {...props}
       />
       <label
         htmlFor={id}
-        className={`absolute bg-white px-[6px] left-4 top-1/2 -translate-y-1/2 text-neutral-400 text-sm transition-all duration-500
-        peer-focus:text-primary-100 peer-focus:text-xs peer-focus:left-5 peer-focus:top-1 peer-focus:-translate-y-3/4
-        overflow-hidden z-5 ${
-          value ? "left-5 top-1 -translate-y-2/3 text-xs" : ""
-        }`}
+        className={`absolute bg-white text-xs xs:text-[13px] md:text-sm lg:text-sm px-[6px] left-4 top-1/2 -translate-y-1/2 text-neutral-400 transition-all duration-500
+        peer-focus:text-primary-100 ${
+          value !== "" ? "text-xs left-5 -top-[1px]" : ""
+        } peer-focus:text-xs peer-focus:left-5 peer-focus:-top-[1px]
+        overflow-hidden z-5 `}
       >
         {label}
       </label>
