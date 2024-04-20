5e822b82556936189ab3678ef6d6831ebf0e3360
baeseongjae <3574214@naver.com>
2024-04-20 21:23:46 +0900

    ♻️ refactor: 로그인 모달 리팩토링 -> 로그인 폼 컴포넌트 분리

--- src/app/(providers)/(root)/auth/_components/LogInModal/LogInModal.tsx
+++ src/app/(providers)/(root)/auth/_components/LogInModal/LogInModal.tsx
@@ -1,95 +1,15 @@
-import API from "@/api/index.api";
-import { SubmitButton, VisibleToggleButton } from "@/components/Button";
 import Heading from "@/components/Heading";
-import AuthInput from "@/components/Input/AuthInput";
 import Modal from "@/components/Modal";
-import { useAuth, useModal, useUser } from "@/contexts";
-import { useMutation } from "@tanstack/react-query";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
-import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import HrTag from "./_components/HrTag";
-import SocialLogInSection from "./_components/SocialLogInSection";
+import { HrTag, LogInForm, SocialLogInSection } from "./_components";
 
 function LogInModal({ pathToGo }: { pathToGo?: string }) {
-  const auth = useAuth();
-  const user = useUser();
-  const modal = useModal();
-  const router = useRouter();
-  const { mutateAsync: logIn } = useMutation({ mutationFn: API.auth.logIn });
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
-
-  const handleClickLogIn = async () => {
-    if (!email.trim()) return toast.error("이메일을 입력해 주세요");
-    if (!password.trim()) return toast.error("비밀번호를 입력해 주세요");
-
-    try {
-      await logIn({ email, password });
-      auth.setIsLoggedIn(true);
-      user.setEmail(email);
-      toast.success("로그인 성공!");
-      if (pathToGo) {
-        router.push(`${pathToGo}`);
-      }
-      modal.close();
-    } catch (e) {
-      toast.error("로그인 실패!");
-    }
-  };
-
   return (
     <Modal className="px-5 py-[27px] lg:px-5 lg:max-w-[390px] text-neutral-90">
       <Heading className="text-center my-4 mb-8 text-xl xs:text-xl md:text-[22px] lg:text-2xl">
         로그인
       </Heading>
-      <form
-        className="flex flex-col gap-y-4 rounded-lg"
-        onSubmit={(e) => e.preventDefault()}
-      >
-        <ul className="flex flex-col gap-y-3">
-          <li className="flex flex-col gap-y-1 relative">
-            <AuthInput
-              label="이메일"
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </li>
-          <li className="flex flex-col gap-y-1 relative">
-            <AuthInput
-              label="비밀번호"
-              type={isPasswordVisible ? "text" : "password"}
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <VisibleToggleButton
-              isPasswordVisible={isPasswordVisible}
-              setIsPasswordVisible={setIsPasswordVisible}
-            />
-          </li>
-        </ul>
-
-        <SubmitButton
-          onClick={handleClickLogIn}
-          className="text-sm md:text-[15px] mt-5"
-        >
-          로그인하기
-        </SubmitButton>
-        <Link
-          href="/auth/sign-up"
-          onClick={() => modal.close()}
-          className="text-primary-100 text-[11px] md:text-[13px] text-center outline-none rounded-md focus:border focus:border-primary-100"
-        >
-          아직 회원이 아니신가요?
-          <span className="ml-1 md:ml-2 underline font-bold hover:text-primary-100/50">
-            가입하기
-          </span>
-        </Link>
-      </form>
+      <LogInForm pathToGo={pathToGo} />
       <HrTag />
       <SocialLogInSection />
     </Modal>
---
+++ src/app/(providers)/(root)/auth/_components/LogInModal/_components/index.ts
@@ -0,0 +1,3 @@
+export { default as HrTag } from "./HrTag";
+export { default as LogInForm } from "./LogInForm";
+export { default as SocialLogInSection } from "./SocialLogInSection";
