c623464072d8960d276babf85b4fc410182a1328
baeseongjae <3574214@naver.com>
2024-05-03 18:10:57 +0900

    ♻️ refactor: 내가 쓴 판매글 페이지 - 로그인 상태에 따른 처리 추가

--- src/react-query/my/useQueryMyDeals.tsx
+++ src/react-query/my/useQueryMyDeals.tsx
@@ -1,9 +1,13 @@
 import API from "@/api/index.api";
+import { useAuth } from "@/contexts";
 import { useQuery } from "@tanstack/react-query";
 
 export default function useQueryMyDeals() {
+  const { isLoggedIn } = useAuth();
+
   return useQuery({
     queryKey: ["myDeals"],
     queryFn: API.deals.getMyDeals,
+    enabled: isLoggedIn,
   });
 }
