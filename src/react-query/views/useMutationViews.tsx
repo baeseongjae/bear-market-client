45cce8c34c92ce451b01a0832199815bde7f48e3
baeseongjae <3574214@naver.com>
2024-05-08 17:30:29 +0900

    ♻️ refactor: 조회수 업데이트 - 업데이트 시, 쿼리 invalidate 추가

--- src/react-query/views/useMutationViews.tsx
+++ src/react-query/views/useMutationViews.tsx
@@ -1,8 +1,16 @@
 import API from "@/api/index.api";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-export default function useMutationViews() {
+export default function useMutationViews(dealId: number) {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: API.deals.updateViews,
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        exact: true,
+        queryKey: ["deal", dealId],
+      });
+    },
   });
 }
